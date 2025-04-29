3293d63facaa347f07d6a32a41d20703830b7f8a
Dmitrii <57145783+inskyeee@users.noreply.github.com>
2025-04-29 15:12:00 +0100

    Set up templates architecture

--- src/main/java/com/example/blog/config/AppConfig.java
+++ src/main/java/com/example/blog/config/AppConfig.java
@@ -1,8 +1,22 @@
 package com.example.blog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+
 
 @Configuration
 public class AppConfig {
-    
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
+
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
 }
+    
